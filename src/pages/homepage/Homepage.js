33f928e7f99baf9756064f893b00a3b1967cd767
Nuccino92 <anthnucci@hotmail.com>
2022-06-26 20:14:28 -0400

    added state functions

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,9 +1,32 @@
-import "./Homepage";
+import HomeButton from "../../styledComponents/HomeButton.styled";
+import "./Homepage.scss";
 
-const Homepage = () => {
+const Homepage = ({ setLogin, setRanked, setCasual }) => {
+  const isAuthenticated = true;
+
+  const handleCasual = () => {
+    setCasual(true);
+  };
+
+  const handleRanked = () => {
+    if (!isAuthenticated) return setLogin(true);
+    setRanked(true);
+  };
+
   return (
     <div className="Homepage-component" data-testid="homepage">
-      Homepage
+      <h1>this is the homepage</h1>
+      <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+        et quas molestias excepturi sint occaecati cupiditate non provident,
+        similique sunt in culpa qui officia deserunt mollitia animi, id est
+        laborum et dolorum fuga.
+      </p>
+      <div>
+        <HomeButton onClick={handleCasual}>Casual</HomeButton>
+        <HomeButton onClick={handleRanked}>Ranked</HomeButton>
+      </div>
     </div>
   );
 };

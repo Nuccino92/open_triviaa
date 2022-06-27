1235664da8a04eb79572b8f7171c07e6fbb2f6d2
Nuccino92 <anthnucci@hotmail.com>
2022-06-26 20:13:26 -0400

    adjusted nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,25 +1,35 @@
 import "./Nav.scss";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/open-trivia-logo.png";
 
-const Nav = () => {
+const Nav = ({ setLogin, setRegister }) => {
   const user = false;
   return (
-    <nav className="Nav">
+    <nav className="Nav-component">
       {user ? (
-        <ul>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
+        <>
+          <Link to="/">
+            <img src={logo} alt="Open Trivia Logo home button" />
+          </Link>
+          <ul>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/leaderboards">Leaderboards</Link>
+            </li>
+          </ul>
+        </>
       ) : (
-        <ul>
-          <li>
-            <Link to="/login">Log in</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
+        <>
+          <Link to="/">
+            <img src={logo} alt="Open Trivia Logo home button" />
+          </Link>
+          <ul>
+            <li onClick={setLogin}>Log in</li>
+            <li onClick={setRegister}>Register</li>
+          </ul>
+        </>
       )}
     </nav>
   );

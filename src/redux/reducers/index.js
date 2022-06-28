b478e45e933e5ac3bb012c01ea50a58c56fb9e6b
Nuccino92 <anthnucci@hotmail.com>
2022-06-27 23:07:29 -0400

    added error reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user";
 import gameReducer from "./game";
+import errorReducer from "./errors";
 
 export default combineReducers({
   userReducer,
   gameReducer,
+  errorReducer,
 });

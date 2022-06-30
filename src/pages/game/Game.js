a6cd75d102f3130620c8acd7a5b168f846a59bc9
Nuccino92 <anthnucci@hotmail.com>
2022-06-29 23:08:31 -0400

    added game timer component

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,5 +1,19 @@
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+
+import Timer from "./Timer";
+
 const Game = () => {
-  return <div>this is the game</div>;
+  const { mode, questions } = useSelector((state) => state.gameReducer);
+
+  return (
+    <div>
+      <h1>{mode.toUpperCase()}</h1>
+      <div className="timer">
+        <Timer />
+      </div>
+    </div>
+  );
 };
 
 export default Game;

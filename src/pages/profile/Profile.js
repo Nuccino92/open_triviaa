adf06038b26576676bc919a93dfc2698df310776
Nuccino92 <anthnucci@hotmail.com>
2022-07-06 23:34:28 -0400

    added intial profile info

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,9 +1,50 @@
-import "./Profile";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Profile.scss";
 
 const Profile = () => {
+  const [data, setData] = useState({
+    picture: "https://c.tenor.com/NICoVNbKVGYAAAAM/profile-picture.gif",
+    username: "JohnCena22",
+    email: "johnnyboy@gmail.com",
+  });
+
+  const [scores, setScores] = useState([
+    {
+      category: "General Knowledge",
+      time: "03:07:38",
+      score: 9,
+    },
+    {
+      category: "Anime & Manga",
+      time: "02:27:78",
+      score: 7,
+    },
+    {
+      category: "Sports",
+      time: "01:55:24",
+      score: 12,
+    },
+  ]);
+
   return (
     <div className="Profile-component" data-testid="profile">
-      Profile
+      <img src={data.picture} alt="dssd"></img>
+      <h2>{data.username}</h2>
+
+      <h4>Top 3 Categories</h4>
+      {scores.map((score, index) => {
+        return (
+          <div key={index}>
+            <h5>{score.category}</h5>
+            <span>{score.score} / 15 </span>
+            <span>{score.time}</span>
+          </div>
+        );
+      })}
+      <Link to="/profile/highscores">
+        <button>View Scores</button>
+      </Link>
     </div>
   );
 };

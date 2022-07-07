b2605bb32da47e7b7d83c1581814e64856d64115
Nuccino92 <anthnucci@hotmail.com>
2022-07-06 23:34:14 -0400

    removed categories

--- src/resources/fetchCasualQuestions.js
+++ src/resources/fetchCasualQuestions.js
@@ -1,13 +1,13 @@
 const fetchCasualQuestions = async (setupInformation) => {
-  const { amount, category, difficulty, type } = setupInformation;
+  const { amount, category } = setupInformation;
 
-  const difficultyCondition =
-    difficulty !== "" ? `&difficulty=${difficulty}` : "";
+  // const difficultyCondition =
+  //   difficulty !== "" ? `&difficulty=${difficulty}` : "";
 
-  const typeCondition = type !== "" ? `&type=${type}` : "";
+  // const typeCondition = type !== "" ? `&type=${type}` : "";
 
   const response = await fetch(
-    `https://opentdb.com/api.php?amount=${amount}&category=${category}${difficultyCondition}${typeCondition}`
+    `https://opentdb.com/api.php?amount=${amount}&category=${category}`
   );
   const questions = await response.json();
 

3b4ff8cade19981562ee05e589676bb6a4e4f6d7
Nuccino92 <anthnucci@hotmail.com>
2022-07-09 19:27:28 -0400

    added links

--- src/pages/leaderboards/Leaderboards.js
+++ src/pages/leaderboards/Leaderboards.js
@@ -1,21 +1,23 @@
 import "./Leaderboards.scss";
+import { Link } from "react-router-dom";
 
 const Leaderboards = () => {
   return (
     <div className="Leaderboards-component">
+      <h2>Select A Category</h2>
       <ul>
-        <li>General Knowledge</li>
-        <li>Books</li>
-        <li>Film</li>
-        <li>Music</li>
-        <li>Television</li>
-        <li>Video Games</li>
-        <li>Science & Nature</li>
-        <li>Computers</li>
-        <li>Sports</li>
-        <li>Geography</li>
-        <li>History</li>
-        <li>Anime & Manga</li>
+        <Link to="/leaderboards/General Knowledge">General Knowledge</Link>
+        <Link to="/leaderboards/Books">Books</Link>
+        <Link to="/leaderboards/Film">Film</Link>
+        <Link to="/leaderboards/Music">Music</Link>
+        <Link to="/leaderboards/Television">Television</Link>
+        <Link to="/leaderboards/Video Games">Video Games</Link>
+        <Link to="/leaderboards/Science & Nature">Science & Nature</Link>
+        <Link to="/leaderboards/Computers">Computers</Link>
+        <Link to="/leaderboards/Sports">Sports</Link>
+        <Link to="/leaderboards/Geography">Geography</Link>
+        <Link to="/leaderboards/History">History</Link>
+        <Link to="/leaderboards/Anime & Manga">Anime & Manga</Link>
       </ul>
     </div>
   );

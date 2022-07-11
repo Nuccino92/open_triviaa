f3abb8b296a39bd59b92f2213119234fddbdd8f4
Nuccino92 <anthnucci@hotmail.com>
2022-07-10 20:00:55 -0400

    added modal functionality

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import EditProfile from "../../components/editProfile/EditProfile";
 import "./Profile.scss";
-import herd from "./herd.jpg";
 
 const Profile = () => {
+  const [editModal, setEditModal] = useState(false);
+
   const [data] = useState({
-    picture: herd,
     username: "JohnCena22",
     email: "johnnyboy@gmail.com",
   });
@@ -28,31 +29,46 @@ const Profile = () => {
     },
   ]);
 
+  const handleClick = (e) => {
+    console.log(e.target.className);
+    if (e.target.className.includes("Profile-component")) setEditModal(false);
+  };
+
+  useEffect(() => {
+    console.log(editModal);
+  }, [editModal]);
+
   return (
-    <div className="Profile-component" data-testid="profile">
-      <div>
-        <img src={data.picture} alt="dssd"></img>
+    <div
+      onMouseDown={handleClick}
+      className="Profile-component"
+      data-testid="profile"
+    >
+      <div className="user-profile-container">
         <h2>{data.username}</h2>
-        <Link to="/profile/highscores">
-          <button>Game History</button>
-        </Link>
-        <button>Edit Profile</button>
+        <button onClick={() => setEditModal(true)} className="edit-profile">
+          Edit Profile
+        </button>
       </div>
-      <div>
+      <div className="top-3-games-container">
+        <h4>Top 3 Games</h4>
         <div className="top-3-games">
-          <h4>Top 3 Games</h4>
           {scores.map((score, index) => {
             return (
               <div key={index}>
-                <h5>{score.category}</h5>
+                <h5>Category: {score.category}</h5>
                 <span>{score.score} / 15 </span>
                 <span>{score.time}</span>
-                <Link to="/game/gamelog">View Game Log</Link>
+                <Link to="/game/gamelog">Game Log</Link>
               </div>
             );
           })}
         </div>
+        <Link to="/profile/highscores">
+          <button>Game History</button>
+        </Link>
       </div>
+      {editModal && <EditProfile />}
     </div>
   );
 };

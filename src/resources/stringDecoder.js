957646d0e2bb96a7617c4931a35f84d44f8acbbb
Nuccino92 <anthnucci@hotmail.com>
2022-07-10 20:00:37 -0400

    es6 syntax

--- src/resources/stringDecoder.js
+++ src/resources/stringDecoder.js
@@ -1,7 +1,7 @@
 // prints out proper text, decodes all HTML entities
 
 const stringDecoder = (string) => {
-  var textArea = document.createElement("textarea");
+  let textArea = document.createElement("textarea");
   textArea.innerHTML = string;
   return textArea.value;
 };

d2e9930613537194ed4a989f9165a47e40e3bcb4
Nuccino92 <anthnucci@hotmail.com>
2022-07-12 19:43:38 -0400

    completed edit profile modal

--- src/components/editProfile/EditProfile.js
+++ src/components/editProfile/EditProfile.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import FormModal from "../../styledComponents/FormModal.styled";
 
 const EditProfile = () => {
-  return <FormModal>sadasd</FormModal>;
+  const [data] = useState({
+    username: "JohnCena22",
+    email: "johnnyboy@gmail.com",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <FormModal className="EditProfile-component">
+      <main>
+        <h2>Edit Profile</h2>
+        <form>
+          <input name="username" value={data.username} />
+          <input name="email" value={data.email} />
+          <input name="password" placeholder="password" />
+          <input name="retype password" placeholder="retype password" />
+          <button type="submit" onClick={handleSubmit}>
+            Update
+          </button>
+        </form>
+      </main>
+    </FormModal>
+  );
 };
 
 export default EditProfile;

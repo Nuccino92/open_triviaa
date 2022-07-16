7053b56d7f3a68edd655a8f8a4d89ac3e5e058f6
Nuccino92 <anthnucci@hotmail.com>
2022-07-15 22:32:15 -0400

    added redux provider to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./redux/store";
 
 test("renders App", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });

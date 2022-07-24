d63b0c2bd2537f37e89b49696839e139b701f002
Nuccino92 <anthnucci@hotmail.com>
2022-07-24 19:36:23 -0400

    User model and controller

---
+++ server/demo/src/main/java/com/example/demo/User/User.java
@@ -0,0 +1,60 @@
+package com.example.demo.User;
+
+public class User {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    // overloaded constructors 
+    public User() {
+        
+    }
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ server/demo/src/main/java/com/example/demo/User/UserController.java
@@ -0,0 +1,5 @@
+package com.example.demo.User;
+
+public class UserController {
+    
+}
